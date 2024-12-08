5d16d548ed69f3d324af5ab9c885fec49fe72b82
NvRSleep <111803646+NvRSleep@users.noreply.github.com>
2024-12-09 01:55:06 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const bio = document.querySelector('#bio');
-  bio.style.opacity = 0;
-  bio.style.transition = 'opacity 1s ease-in-out';
-
-  setTimeout(() => {
-    bio.style.opacity = 1;
-  }, 500);
-});
